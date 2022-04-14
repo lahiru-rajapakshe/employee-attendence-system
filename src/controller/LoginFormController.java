416fedf53ab92e802bcad245b4e2fdd781e50b07
LahiruRJ <sanlahiru720@gmail.com>
2022-04-14 08:38:29 +0530

    Add Login Screen UI

---
+++ src/controller/LoginFormController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class LoginFormController {
+    public void btnClose_OnAction(ActionEvent event) {
+    }
+
+    public void btnSignIn_OnAction(ActionEvent event) {
+    }
+}
