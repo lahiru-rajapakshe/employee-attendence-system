a0238b9f3de2a773458e31147c3da5634e2e80df
LahiruRJ <sanlahiru720@gmail.com>
2022-04-16 20:45:28 +0530

    Impliment switching new form

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -3,10 +3,19 @@ package controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class LoginFormController {
     public Button btnSignIn;
@@ -28,6 +37,37 @@ public class LoginFormController {
             txtUserName.selectAll();
             return;
         }
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement stm = connection.prepareStatement("SELECT name, role FROM user WHERE username=? AND password=?");
+            stm.setString(1, txtUserName.getText().trim());
+            stm.setString(2, txtPassword.getText().trim());
+            ResultSet rst = stm.executeQuery();
+
+            if (rst.next()){
+                SecurityContextHolder.setPrincipal(new Principal(
+                        txtUserName.getText(),
+                        rst.getString("name"),
+                        Principal.UserRole.valueOf(rst.getString("role"))));
+                String path = null;
+
+                if (rst.getString("role").equals("ADMIN")){
+                    path = "/view/AdminHomeForm.fxml";
+                }else{
+                    path = "/view/UserHomeForm.fxml";
+                }
+                FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource(path));
+                AnchorPane root = fxmlLoader.load();
+                Scene homeScene = new Scene(root);
+                Stage primaryStage = (Stage)(btnSignIn.getScene().getWindow());
+                homeScene.getStylesheets().add("view/style/userHomeStyles.css");
+                primaryStage.setScene(homeScene);
+                primaryStage.setTitle("Student Attendance System: Home");
+                Platform.runLater(()-> {
+                    primaryStage.sizeToScene();
+                    primaryStage.centerOnScreen();
+                });
+            }
 
     }
 }
