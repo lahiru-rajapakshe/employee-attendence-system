1fc55c5cc3005700c5dfb114655c7d4939bf9ca4
LahiruRJ <sanlahiru720@gmail.com>
2022-04-18 09:41:53 +0530

    Create DBConnection class

---
+++ src/db/DBConnection.java
@@ -0,0 +1,12 @@
+package db;
+
+import java.sql.Connection;
+
+public class DBConnection {
+    private static DBConnection dbConnection;
+    private Connection connection;
+
+    private DBConnection(){
+
+    }
+}
