13208f605a7eb813008d158a75532ca2c3205cdf
LahiruRJ <sanlahiru720@gmail.com>
2022-04-18 21:51:37 +0530

    Implment initialize method userHomeForm

--- src/controller/UserHomeFormController.java
+++ src/controller/UserHomeFormController.java
@@ -1,4 +1,58 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import security.SecurityContextHolder;
+
 public class UserHomeFormController {
+    public Button btnRecordAttendance;
+    public Button btnViewReports;
+    public Button btnUserProfile;
+    public Button btnSignOut;
+    public Label lblHover;
+    public Label lblGreeting;
+    public AnchorPane root;
+    public AnchorPane mainlbl;
+    public Label time;
+    public AnchorPane dateAnc;
+    public AnchorPane dateAnc2;
+    public JFXButton btnHomeClose;
+    public JFXButton btnMinimize;
+    private int minute;
+    private int hour;
+    private int second;
+
+
+    public void initialize(){
+
+        final String initialText = lblHover.getText();
+        btnRecordAttendance.setOnMouseEntered(event -> displayHoveringText((Button) event.getSource()));
+        btnViewReports.setOnMouseEntered(event -> displayHoveringText((Button) event.getSource()));
+        btnUserProfile.setOnMouseEntered(event -> displayHoveringText((Button) event.getSource()));
+        btnSignOut.setOnMouseEntered(event -> displayHoveringText((Button) event.getSource()));
+
+        btnRecordAttendance.setOnMouseExited(event -> lblHover.setText(initialText));
+        btnViewReports.setOnMouseExited(event -> lblHover.setText(initialText));
+        btnUserProfile.setOnMouseExited(event -> lblHover.setText(initialText));
+        btnSignOut.setOnMouseExited(event -> lblHover.setText(initialText));
+
+        lblGreeting.setText("Hello " + SecurityContextHolder.getPrincipal().getName() + " !");
+
+        root.setOnKeyReleased(event -> {
+            switch(event.getCode()){
+                case F1:
+                    btnRecordAttendance.fire();
+                    break;
+                case F12:
+                    btnSignOut.fire();
+                    break;
+            }
+        });
+
+      //  showdate();
+    }
+
+
 }
