bea093e1068bb2f30cb6cb8aab02afffa3263e93
LahiruRJ <sanlahiru720@gmail.com>
2022-04-18 09:42:32 +0530

    Set ini method dbconnection class

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -9,4 +9,14 @@ public class DBConnection {
     private DBConnection(){
 
     }
+
+    public void init(Connection connection){
+        if (this.connection == null){
+            this.connection = connection;
+        }else if (this.connection != connection){
+            throw new RuntimeException("Connection has been already initialized");
+        }
+    }
+
+
 }
