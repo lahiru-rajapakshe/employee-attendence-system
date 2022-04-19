4cda292a763ded0c235c2fb4cab668beec545bd6
LahiruRJ <sanlahiru720@gmail.com>
2022-04-19 23:08:53 +0530

    Implement Initialize method RecordAttendanceSystem

--- src/controller/RecordAttendanceFormController.java
+++ src/controller/RecordAttendanceFormController.java
@@ -1,12 +1,22 @@
 package controller;
 
+import db.DBConnection;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.Date;
 
 public class RecordAttendanceFormController {
     public TextField txtStudentID;
@@ -22,5 +32,39 @@ public class RecordAttendanceFormController {
     private PreparedStatement stmSearchStudent;
     private Student student;
 
+    public void initialize() {
+        btnIn.setDisable(true);
+        btnOut.setDisable(true);
+        lblDate.setText(String.format("%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %1$Tp", new Date()));
 
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), (event -> {
+            lblDate.setText(String.format("%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %1$Tp", new Date()));
+        })));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+
+        updateLatestAttendance();
+
+        Connection connection = DBConnection.getInstance().getConnection();
+        try {
+            stmSearchStudent = connection.prepareStatement("SELECT * FROM student WHERE id=?");
+        } catch (Exception e) {
+            new DepAlert(Alert.AlertType.WARNING, "Failed to connect with DB", "Connection Error", "Error").show();
+            e.printStackTrace();
+            Platform.runLater(() -> {
+                ((Stage) (btnIn.getScene().getWindow())).close();
+            });
+        }
+
+        root.setOnKeyReleased(event -> {
+            switch (event.getCode()) {
+                case F10:
+                    btnIn.fire();
+                    break;
+                case ESCAPE:
+                    btnOut.fire();
+                    break;
+            }
+        });
+    }
 }
