234f1a08a3eb9794fb73f6a4a8e7e442474906cb
LahiruRJ <sanlahiru720@gmail.com>
2022-04-19 23:09:32 +0530

    Implement brnIn

--- src/controller/RecordAttendanceFormController.java
+++ src/controller/RecordAttendanceFormController.java
@@ -5,6 +5,7 @@ import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -66,5 +67,9 @@ public class RecordAttendanceFormController {
                     break;
             }
         });
+    }
+
+    public void btnIn_OnAction(ActionEvent event) {
+        recordAttendance(true);
     }
 }
