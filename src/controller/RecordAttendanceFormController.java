a522f6b6f98489bc7c2c03f55b2d6e105ccb660a
LahiruRJ <sanlahiru720@gmail.com>
2022-04-19 23:07:53 +0530

    Initialize basic vars

--- src/controller/RecordAttendanceFormController.java
+++ src/controller/RecordAttendanceFormController.java
@@ -1,4 +1,26 @@
 package controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+import java.sql.PreparedStatement;
+
 public class RecordAttendanceFormController {
+    public TextField txtStudentID;
+    public ImageView imgProfile;
+    public Button btnIn;
+    public Button btnOut;
+    public Label lblDate;
+    public Label lblID;
+    public Label lblName;
+    public Label lblStatus;
+    public Label lblStudentName;
+    public AnchorPane root;
+    private PreparedStatement stmSearchStudent;
+    private Student student;
+
+
 }
