20636e1b9b9e701bfec44f041541bf66632176f3
LahiruRJ <sanlahiru720@gmail.com>
2022-04-20 22:43:56 +0530

    Add basic components to the ImportDBClass

--- src/controller/ImportDBFormController.java
+++ src/controller/ImportDBFormController.java
@@ -1,4 +1,21 @@
 package controller;
 
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+import java.io.File;
+
 public class ImportDBFormController {
+    public RadioButton rdoRestore;
+    public ToggleGroup abc;
+    public TextField txtBrowse;
+    public Button btnBrowse;
+    public RadioButton rdoFirstTime;
+    public Button btnOK;
+    private SimpleObjectProperty<File> fileProperty;
+
+
 }
