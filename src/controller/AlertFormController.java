cb88617b1d00b8a7f4d792065045987d746c05af
LahiruRJ <sanlahiru720@gmail.com>
2022-04-23 23:24:48 +0530

    Initialize the variables alert form controller

--- src/controller/AlertFormController.java
+++ src/controller/AlertFormController.java
@@ -1,4 +1,34 @@
 package controller;
 
+import javafx.animation.ScaleTransition;
+import javafx.application.Platform;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 public class AlertFormController {
+    public Button btnProceed;
+    public Button btnCallPolice;
+    public Label lblId;
+    public Label lblName;
+    public Label lblDate;
+    public ImageView imgDanger;
+    public Label lblAlert;
+    public Label lblDescription;
+    private SimpleBooleanProperty record;
+    private MediaPlayer player;
+
+
 }
