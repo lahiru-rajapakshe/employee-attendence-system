c3df3972af85d966ed90c03bde74b03d552e0482
LahiruRJ <sanlahiru720@gmail.com>
2022-04-23 23:31:35 +0530

    Implement filechooser method

--- src/controller/BackupAndRestoreFormController.java
+++ src/controller/BackupAndRestoreFormController.java
@@ -1,7 +1,28 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.stage.FileChooser;
+import util.RJAlert;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
 
 public class BackupAndRestoreFormController {
     public Button btnBackup;
+
+    public void btnBackup_OnAction(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose backup location");
+        fileChooser.setInitialFileName(LocalDate.now() + "-sas-bak");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Backup files (*.dep8bak)", "*.dep8bak"));
+        File file = fileChooser.showSaveDialog(btnBackup.getScene().getWindow());
+
+
+    }
+
+
 }
