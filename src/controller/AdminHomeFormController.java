77ed3756459e16900d66b38053c1d9c527543206
LahiruRJ <sanlahiru720@gmail.com>
2022-04-24 11:20:42 +0530

    Initilalized variables

--- src/controller/AdminHomeFormController.java
+++ src/controller/AdminHomeFormController.java
@@ -1,4 +1,19 @@
 package controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
 public class AdminHomeFormController {
+    public Button btnRecordAttendance;
+    public Button btnViewReports;
+    public Button btnUserProfile;
+    public Button btnManageUsers;
+    public Button btnBackupRestore;
+    public Button btnSignOut;
+    public Label lblHover;
+    public Label lblGreeting;
+    public AnchorPane root;
+
+
 }
