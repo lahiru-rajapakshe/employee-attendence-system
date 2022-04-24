b97588e84052de2c097d7033d01f6d8bf0c477ae
LahiruRJ <sanlahiru720@gmail.com>
2022-04-24 11:21:34 +0530

    Add basic styles for adminHomeForm

--- src/controller/AdminHomeFormController.java
+++ src/controller/AdminHomeFormController.java
@@ -3,6 +3,7 @@ package controller;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import security.SecurityContextHolder;
 
 public class AdminHomeFormController {
     public Button btnRecordAttendance;
@@ -14,6 +15,24 @@ public class AdminHomeFormController {
     public Label lblHover;
     public Label lblGreeting;
     public AnchorPane root;
+
+
+    public void initialize(){
+        final String initialText = lblHover.getText();
+        btnRecordAttendance.setOnMouseEntered(event -> displayHoveringText((Button) event.getSource()));
+        btnViewReports.setOnMouseEntered(event -> displayHoveringText((Button) event.getSource()));
+        btnUserProfile.setOnMouseEntered(event -> displayHoveringText((Button) event.getSource()));
+        btnManageUsers.setOnMouseEntered(event -> displayHoveringText((Button) event.getSource()));
+        btnBackupRestore.setOnMouseEntered(event -> displayHoveringText((Button) event.getSource()));
+        btnSignOut.setOnMouseEntered(event -> displayHoveringText((Button) event.getSource()));
+
+        btnRecordAttendance.setOnMouseExited(event -> lblHover.setText(initialText));
+        btnViewReports.setOnMouseExited(event -> lblHover.setText(initialText));
+        btnUserProfile.setOnMouseExited(event -> lblHover.setText(initialText));
+        btnManageUsers.setOnMouseExited(event -> lblHover.setText(initialText));
+        btnBackupRestore.setOnMouseExited(event -> lblHover.setText(initialText));
+        btnSignOut.setOnMouseExited(event -> lblHover.setText(initialText));
+
 
 
 }
