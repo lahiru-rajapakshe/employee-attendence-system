bde134c5d44fe1531a97c04539db050320dfd97c
LahiruRJ <sanlahiru720@gmail.com>
2022-04-27 21:34:43 +0530

    set Initializers

--- src/controller/CreateAdminFormController.java
+++ src/controller/CreateAdminFormController.java
@@ -1,4 +1,31 @@
 package controller;
 
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+
 public class CreateAdminFormController {
+
+    private void handlePasswordStrength() {
+        rect1.setFill(Color.TRANSPARENT);
+        rect2.setFill(Color.TRANSPARENT);
+        rect3.setFill(Color.TRANSPARENT);
+
+        txtPassword.textProperty().addListener((observable, oldValue, newValue) -> {
+            rect1.setFill(Color.TRANSPARENT);
+            rect2.setFill(Color.TRANSPARENT);
+            rect3.setFill(Color.TRANSPARENT);
+            if (!newValue.trim().isEmpty() && newValue.trim().length() < 4) {
+                rect1.setFill(Paint.valueOf("#ff1f1f"));
+            } else if (newValue.trim().length() == 4) {
+                rect1.setFill(Paint.valueOf("#ff1f1f"));
+                rect2.setFill(Paint.valueOf("#ffef21"));
+            } else if (newValue.trim().length() >= 4) {
+                rect1.setFill(Paint.valueOf("#ff1f1f"));
+                rect2.setFill(Paint.valueOf("#ffef21"));
+                rect3.setFill(Paint.valueOf("#21ff56"));
+            }
+        });
+    }
+
+
 }
